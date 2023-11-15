c4d08641380e38586a241676143a9eb81aad5274
Nurbek Abdirasulov <>
2023-11-16 03:37:37 +0600

    first commit

---
+++ src/main/java/com/codewise/CodewiseApplication.java
@@ -0,0 +1,13 @@
+package com.codewise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodewiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodewiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewise/CodewiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodewiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
